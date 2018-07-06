f93d0f50c5857e5ffe4067059fbae8a0b5c8d7bc
Jephte Drake Colin <drakecolinj@gmail.com>
2018-07-06 10:13:55 -0400

    AsyncHttpClient implemented
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/flixx/MainActivity.java
+++ app/src/main/java/com/realty/drake/flixx/MainActivity.java
@@ -3,11 +3,34 @@ package com.realty.drake.flixx;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url =
+                "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
     }
 }
