1094895a6e66e50e8f6e6a0dac50f6da934f9c41
Jephte Drake Colin <drakecolinj@gmail.com>
2018-07-09 02:52:05 -0400

    YoutubePlayer activity created
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

---
+++ app/src/main/java/com/realty/drake/flixx/YouTubePlayerActivity.java
@@ -0,0 +1,13 @@
+package com.realty.drake.flixx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YouTubePlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_you_tube_player);
+    }
+}
